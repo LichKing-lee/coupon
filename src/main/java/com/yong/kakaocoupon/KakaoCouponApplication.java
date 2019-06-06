94c2ec8ad8f914087e0ebe8af4f55707f4e9a05d
ChangYong <lcy9002@naver.com>
2019-06-06 12:21:45 +0900

    Initialized

---
+++ src/main/java/com/yong/kakaocoupon/KakaoCouponApplication.java
@@ -0,0 +1,13 @@
+package com.yong.kakaocoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoCouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoCouponApplication.class, args);
+	}
+
+}
